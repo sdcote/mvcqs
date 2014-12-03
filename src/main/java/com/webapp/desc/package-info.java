0786eb48aa4c8391a1bdfcb3d290fb1e6adc6fdc
Steve Cote <sdcote@gmail.com>
2014-12-03 09:55:34 -0500

    Start of package documentation

---
+++ src/main/java/com/webapp/desc/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This is a collection of classes which describe the current state of the 
+ * system including all the features, their versions and meta data describing 
+ * their individual state and relationship to other features. 
+ */
+package com.webapp.desc;
